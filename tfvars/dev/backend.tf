3dc2b01a2cfe43d3aa81e6ec8605c5330a5b7619
Arpitha <Shettyarpithasg@gmail.com>
2026-03-28 01:24:14 +0530

    added new files

--- tfvars/dev/backend.tf
+++ tfvars/dev/backend.tf
@@ -1,13 +1,3 @@
- terraform {
-  backend "s3" {
-    bucket         = "shetty-remote-state-dev"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "starshetty-locking-dev"
-  }
-}
-
-
 terraform {
   backend "s3" {
     bucket         = "shetty-remote-state-dev"
