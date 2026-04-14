d41fc876f99a105c4fbb3bdb825da004d183cf1a
Arpitha <Shettyarpithasg@gmail.com>
2026-04-14 22:14:03 +0530

    added new files

--- tfvars/dev/backend.tf
+++ tfvars/dev/backend.tf
@@ -4,5 +4,6 @@ terraform {
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "starshetty-locking-dev"
+   # dynamodb_table = "starshetty-locking-dev"
   }
 }
